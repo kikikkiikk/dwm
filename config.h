ddc57895a67a4859cbcf2d4d4b9399b4c40b4736
Kiki Tan <tanqf7@163.com>
2023-02-07 19:47:03 +0800

    在添加systembar之前的一次备份

--- config.h
+++ config.h
@@ -4,6 +4,11 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad 	    = 10;       /* vertical padding of bar */
@@ -84,15 +89,17 @@ static const char *flameshot[] = {"flameshot","gui",NULL};
 static const char *chrome[] = {"google-chrome-stable",NULL};
 static const char *volumeup[] = {"vol-up",NULL};
 static const char *volumedown[] = {"vol-down",NULL};
-static const char *cwall[] = {"feh","--bg-fill","--no-fehbg","--randomize","~/.feh/*",NULL};
+static const char *cwall[] = {"refresh-bg",NULL};
 static const char *trayer[] = {"tray",NULL};
 static const char *notifc[] = {"notifc",NULL};
+static const char *music[] = {"music",NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_t,	   spawn,          {.v = trayer } },
 	{ MODKEY,	 		XK_n,      spawn,	   {.v = notifc } },
+	{ MODKEY|ShiftMask, 		XK_m,      spawn,	   {.v = music } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -136,8 +143,8 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,		MODKEY,		Button1,	tag,		{0} },
+	{ ClkTagBar,		MODKEY,		Button3,	toggletag,	{0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
